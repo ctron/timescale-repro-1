f47ceb0564b58642db02b2da5349c83b275af0aa
Jens Reimann <jreimann@redhat.com>
2022-05-03 10:13:07 +0200

    add table creation

--- test.sql
+++ test.sql
@@ -1,1 +1,8 @@
+CREATE TABLE temperatures
+(
+    time            TIMESTAMP WITH TIME ZONE NOT NULL,
+    device_id       VARCHAR(64)              NOT NULL,
+    temperature     DOUBLE PRECISION         NOT NULL
+);
+
 \copy temperatures (time, device_id, temperature) from PROGRAM 'mlr --csv cut -f time,device_id,temperature timescale_test.csv' WITH (FORMAT CSV, HEADER)
